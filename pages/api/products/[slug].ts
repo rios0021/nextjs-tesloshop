9edb0718a6dbe6718f26548700fb1204cb94b988
rios0021 <rios0021@algonquinlive.com>
2022-10-08 18:42:40 -0400

    Added restful api

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database'
+import { Product } from '../../../models';
+import { IProduct } from '../../../interfaces/products';
+
+type Data = 
+| IProduct
+| {
+    message: string
+}
+
+export default async function (req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    switch (req.method) {
+        case 'GET':
+            return getProductBySlug(req,res);
+    
+        default:
+            return res.status(400).json({message: 'Bad request'});
+    }
+
+
+}
+
+const getProductBySlug = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const {slug} = req.query;
+    
+    try {
+        await db.connect();
+        const product  = await Product.findOne({slug}).lean();
+        await db.disconnect();
+        if (!product){
+            return res.status(400).json({message: 'No such product'});
+        }
+        return res.status(200).json(product)
+    } catch (error) {
+        await db.disconnect();
+        return res.status(500).json({message: 'Something went wrong'});
+    }
+}
---
+++ pages/api/products/index.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db, SHOP_CONSTANTS } from '../../../database';
+import { IProduct } from '../../../interfaces/products';
+import { Product } from '../../../models';
+
+type Data = 
+| { products: IProduct[]}
+| { message: string;}
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        case 'GET':
+            return getProducts(req, res)
+    
+        default:
+            res.status(400).json({ message: 'Bad Request' })
+    }
+
+}
+
+const getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    try {
+        const {gender = 'all'} = req.query;
+        let condition = {}
+        if(gender != 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)){
+            condition = {gender}
+        }
+        await db.connect();
+        const products = await Product.find(condition).lean().select('title images price inStock slug -_id');
+        await db.disconnect();
+        res.status(200).json({ products })
+    } catch (error) {
+        await db.disconnect();
+        res.status(400).json({ message: 'Something went wrong' })
+    }
+
+}
